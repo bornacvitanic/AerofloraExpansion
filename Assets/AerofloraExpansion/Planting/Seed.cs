18a6399a7604dd8f80b0fbac535f354d43575385
stjepanknezevic <kstjepan4@gmail.com>
2024-03-30 17:26:40 +0100

    feat: add random 90 degrease rotation on spawning

--- Assets/AerofloraExpansion/Planting/Seed.cs
+++ Assets/AerofloraExpansion/Planting/Seed.cs
@@ -7,15 +7,22 @@ namespace AerofloraExpansion.Planting
     public class Seed : MonoBehaviour
     {
         [SerializeField] private GameObject plantPrefabToSpawn;
-        [SerializeField] private LayerMask layerMaskForPlanting;
+        [SerializeField] private LayerMask layerMaskForPlanting = -1;
 
         private void OnCollisionEnter(Collision collision)
         {
             if (plantPrefabToSpawn == null) return;
             if ((layerMaskForPlanting.value & (1 << collision.gameObject.layer)) == 0) return;
 
-            Instantiate(plantPrefabToSpawn, collision.GetContact(0).point, Quaternion.identity);
+            Instantiate(plantPrefabToSpawn, collision.GetContact(0).point, RandomRotationY());
             Destroy(gameObject);
+        }
+
+        private Quaternion RandomRotationY()
+        {
+            float randomY = 90 * Random.Range(0, 3);
+            Vector3 rotation = new Vector3(0, randomY, 0);
+            return Quaternion.Euler(rotation);
         }
     }
 }
