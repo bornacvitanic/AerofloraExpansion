06915fd9119d45ae38cf89b642407d50460e5936
stjepanknezevic <kstjepan4@gmail.com>
2024-03-30 17:13:19 +0100

    feat: added plantableSurface layer and checking logic for it

--- Assets/AerofloraExpansion/Planting/Seed.cs
+++ Assets/AerofloraExpansion/Planting/Seed.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace AerofloraExpansion.Planting
@@ -6,9 +7,13 @@ namespace AerofloraExpansion.Planting
     public class Seed : MonoBehaviour
     {
         [SerializeField] private GameObject plantPrefabToSpawn;
+        [SerializeField] private LayerMask layerMaskForPlanting;
 
         private void OnCollisionEnter(Collision collision)
         {
+            if (plantPrefabToSpawn == null) return;
+            if ((layerMaskForPlanting.value & (1 << collision.gameObject.layer)) == 0) return;
+
             Instantiate(plantPrefabToSpawn, collision.GetContact(0).point, Quaternion.identity);
             Destroy(gameObject);
         }
