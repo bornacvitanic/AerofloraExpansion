ad46f21c3dec2f50ba5046d7bb1b2b557a2d0411
stjepanknezevic <kstjepan4@gmail.com>
2024-03-31 23:19:21 +0200

    feat: add scaling on spawning script

---
+++ Assets/AerofloraExpansion/Spawning/ScalingOnSpawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScalingOnSpawn : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve scaleCurve;
+
+    private void Start()
+    {
+        StartCoroutine(ScaleOverTime());
+    }
+
+    private IEnumerator ScaleOverTime()
+    {
+        float currentTime = 0.0f;
+        while (currentTime <= scaleCurve.keys[^1].time)
+        {
+            float curveValue = scaleCurve.Evaluate(currentTime / scaleCurve.keys[^1].time);
+            transform.localScale = new Vector3(curveValue, curveValue, curveValue);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
